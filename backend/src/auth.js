12382bacfe61d4bd2154ca425daf85ca00fde9a2
ymehili <youssefmehili@gmail.com>
2024-04-05 00:02:18 +0200

    Add mongoose dependency and update docker-compose.yml

--- backend/src/auth.js
+++ backend/src/auth.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
+const mongoose = require('mongoose');
 const { User } = require('./models');
 
 router.post('/auth/register', async (req, res) => {
