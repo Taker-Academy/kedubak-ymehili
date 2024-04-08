49d45f241e89608536a89ea3deeb041c5b9e2139
ymehili <youssefmehili@gmail.com>
2024-04-08 22:35:35 +0200

    Update comment route to include comment ID in the URL

--- backend/src/comment.js
+++ backend/src/comment.js
@@ -4,7 +4,7 @@ const router = express.Router();
 const jwt = require('jsonwebtoken');
 const { User, Post, Comment } = require('./models');
 
-router.post('/comment', async (req, res) => {
+router.post('/comment/:id', async (req, res) => {
     const token = req.headers.authorization.split(' ')[1];
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
 });
