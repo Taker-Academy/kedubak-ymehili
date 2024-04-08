158d67d8d896c407dc53cfddd35d75806ca7731e
ymehili <youssefmehili@gmail.com>
2024-04-08 19:28:54 +0200

    Add JWT verification to comment.js route

--- backend/src/comment.js
+++ backend/src/comment.js
@@ -3,3 +3,10 @@ const bcrypt = require('bcrypt');
 const router = express.Router();
 const jwt = require('jsonwebtoken');
 const { User, Post, Comment } = require('./models');
+
+router.post('/comment', async (req, res) => {
+    const token = req.headers.authorization.split(' ')[1];
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+});
+
+module.exports = router;
